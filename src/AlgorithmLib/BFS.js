34bc20ed9fa5cf60a2039f52a6610445d392d64d
Allen Cao <allencao@berkeley.edu>
2021-07-19 15:55:30 -0700

    switched variable names for better readbility

--- src/AlgorithmLib/BFS.js
+++ src/AlgorithmLib/BFS.js
@@ -2,32 +2,32 @@ import {getAdjacent, getRowFromId, getColFromId, getElementByPos, display} from 
 
 function BFSRun(startRow, startCol, endRow, endCol) {
     let queue = [];
-    let visited = []; // visited will include boxes from scouting adjacents (for performance)
-    let marked = []; // marked will not include boxes from scouting adjacents
+    let marked = []; // marked will include boxes from scouting adjacents (for performance)
+    let visited = []; // visited will not include boxes from scouting adjacents
                      // (so that BFS will stop and not look at adjacents at last node)
     let firstBox = getElementByPos(startRow, startCol)
     queue.push([firstBox]);
-    visited.push(firstBox);
+    marked.push(firstBox);
     while (queue.length != 0) {
         let path = queue[0];
         queue.shift();
         let node = path[path.length - 1];
-        marked.push(node);
+        visited.push(node);
         startRow = getRowFromId(node.id);
         startCol = getColFromId(node.id);
         if (startRow === endRow && startCol === endCol) {
-            display(marked, path);
+            display(visited, path);
             break;
         }
-        let adjacents = getAdjacent(startRow, startCol, visited);
+        let adjacents = getAdjacent(startRow, startCol, marked);
         adjacents.forEach(box => {
             let newPath = path.slice();
             newPath.push(box);
             queue.push(newPath);
-            visited.push(box);
+            marked.push(box);
         });
     }
-    display(marked, []);
+    display(visited, []);
 }
 
 export default BFSRun;
