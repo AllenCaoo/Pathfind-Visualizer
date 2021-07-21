d564632a6e3f50f6f66630a392a8c057bd9604ab
Allen Cao <allencao@berkeley.edu>
2021-07-21 16:41:47 -0700

    Reset back to after state fix attempt

--- src/AlgorithmLib/Dijkstras.js
+++ src/AlgorithmLib/Dijkstras.js
@@ -4,7 +4,7 @@ import {getAdjacent, getRowFromId, getColFromId, getElementByPos,
 
 var visited = []
 var distTo = {}
-var edgeTo = {} // may not be necessary
+// var edgeTo = {} // may not be necessary
 var stack = []
 function DijkstrasRun(startRow, startCol, endRow, endCol) {
     var source = getElementByPos(startRow, startCol);
@@ -12,16 +12,19 @@ function DijkstrasRun(startRow, startCol, endRow, endCol) {
     for (let row = 0; row < allBoxes.length; row++) {
         for (let col = 0; col < allBoxes[0].length; col++) {
             distTo[allBoxes[row][col].id] = Infinity;
-            edgeTo[allBoxes[row][col].id] = null;
+            // edgeTo[allBoxes[row][col].id] = null;
         }
     }
     stack.push([source])
     distTo[source.id] = 0;
-    edgeTo[source.id] = null;
+    // edgeTo[source.id] = null;
     while (stack) {
         let path = stack[0];
         stack.shift();
         let box = path[path.length - 1];
+        if (visited.includes(box)) {
+            continue;
+        }
         visited.push(box);
         if (getRowFromId(box.id) === endRow && getColFromId(box.id) === endCol) {
             display(visited, path);
@@ -42,7 +45,7 @@ function relax(box, path) {
         let oldAdjDist = distTo[adj.id];
         if (newAdjDist < oldAdjDist) {
             distTo[adj.id] = newAdjDist;
-            edgeTo[adj.id] = box.id;
+            // edgeTo[adj.id] = box.id;
         }
         pathCopy.push(adj);
         stack.push(pathCopy);
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,60 +1,36 @@
-import {React, useState, useEffect, Component} from 'react';
+import {React, useState} from 'react';
 
-const Box = ({row, col, color, isStart, isEnd }) => {
+const Box = ({row, col, color}) => {
 
-    var [isWall, toggleState] = useState(false);
+    var [_, toggleState] = useState(false);
 
-    var [initialized, setInitialize] = useState(false);
 
-    function cleanState() {
-        let box = document.getElementById(`${row}-${col}`);
-        if (box.style.backgroundColor === "white") {
-            toggleState(false);
-        } else if (box.style.backgroundColor === "black") {
-            toggleState(true);
+    toggleState = () => {
+        let currColor = document.getElementById(`${row}-${col}`).style.backgroundColor;
+        // if (selectingBegin) {
+        //     let startBox = document.getElementById(`${startPos[0]}-${startPos[1]}`);
+        //     startBox.style.backgroundColor = "white";
+        //     startPos = [row, col];
+        if (currColor === "white") {
+            document.getElementById(`${row}-${col}`).style.backgroundColor = "black";
+        } else if (currColor === "black") {
+            document.getElementById(`${row}-${col}`).style.backgroundColor = "white";
         }
     }
-
-
-    function handleClick() {
-        cleanState();
-        let box = document.getElementById(`${row}-${col}`);
-        if (!isStart && !isEnd) {
-            if (box.style.backgroundColor === "black" 
-            || box.style.backgroundColor === "white") { 
-                toggleState(!isWall);
-            }
-        }
-    }
-
-    useEffect(() => {
-        if (initialized) {
-            let box = document.getElementById(`${row}-${col}`);
-            if (isWall) {
-                box.style.backgroundColor = "black"
-            } else {
-                box.style.backgroundColor = "white";
-            }
-        } else {
-            setInitialize(true);
-        }
-    }, [isWall]);
 
     return (
         <td 
             className="box" 
             id={row + "-" + col}
             style={{backgroundColor: color}} 
-            onClick={ handleClick }
+            onClick={ toggleState } 
             >
         </td>
     )
 }
 
 Box.defaultProps = {
-    color: 'white',
-    isStart: false,
-    isEnd: false
+    color: 'white'
 }
 
 export default Box
