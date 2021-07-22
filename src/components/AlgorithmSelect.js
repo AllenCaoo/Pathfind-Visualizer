a319512f75fcc03ea8aa15e07b8792c43748e8c8
Allen Cao <allencao@berkeley.edu>
2021-07-21 19:37:59 -0700

    Made A* available and changed view of select options

--- src/components/AlgorithmSelect.js
+++ src/components/AlgorithmSelect.js
@@ -7,7 +7,7 @@ const AlgorithmSelect = ( { color, onChange } ) => {
         <select className='slct' id='algorithms' style={{backgroundColor: color}}
             onChange={(evt) => onChange(evt.target.value)} >
             <option value="DI">Dijkstra's</option>
-            <option value="A*">A* (unavailable)</option>
+            <option value="A*">A*</option>
             <option value="DFS">Depth First Search</option>
             <option value="BFS">Breadth First Search</option>
         </select>
