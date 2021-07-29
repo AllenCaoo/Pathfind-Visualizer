0c9fd5bad7e7969222d7542211a0b1973f7003b9
Allen Cao <allencao@berkeley.edu>
2021-07-29 16:48:52 -0700

    Made DFS quicker

--- src/AlgorithmLib/DFS.js
+++ src/AlgorithmLib/DFS.js
@@ -11,6 +11,9 @@ function DFSRun(startRow, startCol, endRow, endCol, orientationalJson) {
         let adjacents = getAdjacent(startRow, startCol, visited, orientationalJson);
         if (!adjacents.length) { return null; }
         for (let i = 0; i < adjacents.length; i++) { // return in for each doesn't end the loop...
+            if (visited.includes(adjacents[i])) {
+                continue;
+            }
             let pathCopy = path.slice();
             let row = getRowFromId(adjacents[i].id);
             let col = getColFromId(adjacents[i].id);
@@ -21,6 +24,7 @@ function DFSRun(startRow, startCol, endRow, endCol, orientationalJson) {
         };
     }
     let DFSPath = DFSHelper(startRow, startCol, endRow, endCol, [], orientationalJson);
+    console.log(visited);
     if (DFSPath) {
         return {"visited": visited, "path": DFSPath};
     }
