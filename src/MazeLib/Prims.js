556117256c29bbab80cd5cbb0457f984945f7b81
Allen Cao <allencao@berkeley.edu>
2021-07-30 16:32:00 -0700

    Accidentally Programmed inverse prims

--- src/MazeLib/Prims.js
+++ src/MazeLib/Prims.js
@@ -1,58 +1,43 @@
 import {getAdjacent, getRowFromId, getColFromId, getElementByPos,
-    getAllBoxes, shuffle, orientationListToJson, nextToVisited} from '../utils';
+    getAllBoxes, shuffle, orientationListToJson, nextToVisited, getFrontier, randNum, getBoxBetween} from '../utils';
 import { maxRow, maxCol } from '../components/Board';
 import { defaultOrientation } from '../components/Settings';
 
 
 export default function runRandomPrims(startRow, startCol) {
-    let orientations = defaultOrientation.slice();
-    var visited = []
-    var distTo = {}
-    // var edgeTo = {} // may not be necessary
-    var stack = []
-    var source = getElementByPos(startRow, startCol);
-    let allBoxes = getAllBoxes();
-    for (let row = 0; row < allBoxes.length; row++) {
-        for (let col = 0; col < allBoxes[0].length; col++) {
-            distTo[allBoxes[row][col].id] = Infinity;
-            // edgeTo[allBoxes[row][col].id] = null;
-        }
+
+    function isBlocked(box) {
+        return blocked.has(box);
     }
-    stack.push([source]);
-    distTo[source.id] = 0;
-    // edgeTo[source.id] = null;
-    while (stack.length != 0) {
+
+    var orientations = defaultOrientation.splice();
+    var blocked = new Set(getAllBoxHorizontal());
+    let blackVisited = getAllBoxHorizontal();
+    let whiteVisited = [getElementByPos(startRow, startCol)];
+    let frontiers = [getElementByPos(startRow, startCol)];
+    while (frontiers.length > 0) {
         shuffle(orientations);
-        let path = stack[0];
-        stack.shift();
-        let box = path[path.length - 1];
-        if (visited.includes(box)) {
-            continue;
+        let index = randNum(0, frontiers.length);
+        let currBox = frontiers[index];
+        frontiers.splice(index, 1)
+        startRow = getRowFromId(currBox.id);
+        startCol = getColFromId(currBox.id);
+        let adjsFrontiers = getFrontier(startRow, startCol, whiteVisited, 
+            orientationListToJson(orientations));
+        if (adjsFrontiers.length > 0) {
+            let frontierBox = adjsFrontiers[0];
+            let betweenBox = getBoxBetween(frontierBox, currBox);
+            whiteVisited.push(currBox);
+            whiteVisited.push(betweenBox);
+            whiteVisited.push(frontierBox);
+            adjsFrontiers.forEach((adj) => {
+                frontiers.push(adj);
+            });
         }
-        visited.push(box);
-        relax(box, path, visited, stack, distTo, orientationListToJson(orientations));
     }
-    return {"visited": getAllBoxHorizontal(), "path": visited};
+    return {"visited": blackVisited, "path": whiteVisited};
 }
 
-
-function relax(box, path, visited, stack, distTo, orientationalJson) {
-    let adjacents = getAdjacent(getRowFromId(box.id), 
-        getColFromId(box.id), visited, orientationalJson, (adj) => {
-             return !nextToVisited(adj, visited, box);
-        });
-    adjacents.forEach(adj => {
-        let pathCopy = path.slice();
-        let newAdjDist = 1;
-        let oldAdjDist = distTo[adj.id];
-        if (newAdjDist < oldAdjDist) {
-            distTo[adj.id] = newAdjDist;
-            // edgeTo[adj.id] = box.id;
-        }
-        pathCopy.push(adj);
-        stack.push(pathCopy);
-    });
-}
 
 function getAllBoxHorizontal() {
     let lst = []
